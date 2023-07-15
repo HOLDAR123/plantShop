c0b3d1d61ae04ff09a56001b5d402bcde1702806
HOLDAR <timurkabravo@gmail.com>
2023-07-15 14:22:49 +0600

    add

---
+++ src/components/footerComp.vue
@@ -0,0 +1,15 @@
+<template >
+    <div class="v-container-footer"> 
+        <div class="v-content">
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'footerComp'
+}
+</script>
+<style >
+    
+</style>
